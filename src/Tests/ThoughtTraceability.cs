0973eb420af2dc4e5fe1fca1f6fb36f9cfdf9a0c
Max Guernsey, III <max@producore.com>
2025-05-19 16:55:38 -0700

    first test cycle

---
+++ src/Tests/ThoughtTraceability.cs
@@ -0,0 +1,22 @@
+﻿using FluentAssertions;
+using Tests.Mocks;
+using ThoughtSharp.Runtime;
+
+namespace Tests
+{
+    [TestClass]
+    public sealed class ThoughtTraceability
+    {
+        [TestMethod]
+        public void CarriesPayload()
+        {
+            var Expected = new MockProduct();
+            
+            var T = Thought.ForProduct(Expected);
+
+            var Actual = T.Unwrap();
+
+            Actual.Should().BeSameAs(Expected);
+        }
+    }
+}
---
+++ src/ThoughtSharp.Runtime/Thought.cs
@@ -0,0 +1,18 @@
+﻿namespace ThoughtSharp.Runtime;
+
+public class Thought
+{
+
+    public static Thought<T> ForProduct<T>(T Product)
+    {
+        return new(Product);
+    }
+}
+
+public class Thought<T>(T Product) : Thought
+{
+    public T Unwrap()
+    {
+        return Product;
+    }
+}
