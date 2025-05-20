1011c1a09a5a5377479ce959fbc7c541b7c0c01a
Max Guernsey, III <max@hexsw.com>
2025-05-20 07:14:43 -0700

    adding behavior

--- src/Tests/ThoughtTraceability.cs
+++ src/Tests/ThoughtTraceability.cs
@@ -1,22 +1,59 @@
-﻿using FluentAssertions;
+﻿// MIT License
+// 
+// Copyright (c) 2024-2024 Hexagon Software LLC
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using FluentAssertions;
 using Tests.Mocks;
 using ThoughtSharp.Runtime;
 
-namespace Tests
+namespace Tests;
+
+[TestClass]
+public sealed class ThoughtTraceability
 {
-    [TestClass]
-    public sealed class ThoughtTraceability
-    {
-        [TestMethod]
-        public void CarriesPayload()
-        {
-            var Expected = new MockProduct();
-            
-            var T = Thought.ForProduct(Expected);
+  MockProduct Expected = null!;
 
-            var Actual = T.Unwrap();
+  [TestInitialize]
+  public void Setup()
+  {
+    Expected = new();
+  }
 
-            Actual.Should().BeSameAs(Expected);
-        }
-    }
+  [TestMethod]
+  public void SetAndGetPayload()
+  {
+    var T = Thought.ForProduct(Expected);
+
+    var Actual = T.GetPayload();
+
+    Actual.Should().BeSameAs(Expected);
+  }
+
+  [TestMethod]
+  public void UseSubthought()
+  {
+    var T = Thought.ForProduct(Expected);
+
+    var Actual = new Thought().Use(T);
+
+    Actual.Should().BeSameAs(Expected);
+  }
 }
