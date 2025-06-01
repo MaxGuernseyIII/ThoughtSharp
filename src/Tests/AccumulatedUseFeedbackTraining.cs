5a91a827d77895a0aab68e45cd70b470a2d1fb58
Max Guernsey, III <max@hexsw.com>
2025-06-01 10:27:05 -0700

    a step toward simplifying use feedback

---
+++ src/Tests/AccumulatedUseFeedbackTraining.cs
@@ -0,0 +1,67 @@
+﻿// MIT License
+// 
+// Copyright (c) 2025-2025 Hexagon Software LLC
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+using FluentAssertions;
+using ThoughtSharp.Runtime;
+using static ThoughtSharp.Runtime.AccumulatedUseFeedback;
+
+namespace Tests;
+
+[TestClass]
+public class AccumulatedUseFeedbackTraining
+{
+  public class MockToUse
+  {
+    public int Operation1CallCount;
+
+    public void Operation1()
+    {
+      Operation1CallCount++;
+    }
+
+    public int Operation2CallCount;
+    public int Operation2SomeArgument;
+
+    public void Operation2(int SomeArgument)
+    {
+      Operation2CallCount++;
+      Operation2SomeArgument = SomeArgument;
+    }
+  }
+
+  [TestMethod]
+  public void HandlesTheDoNothingCase()
+  {
+    var RequiresMore = new BoxedBool() { Value = true };
+    var Source = GetSource<MockToUse>();
+    var ActionSurfaceMock = new MockToUse();
+    var OperationFeedback = new UseFeedback<MockToUse>(ActionSurfaceMock, B => RequiresMore.Value = B);
+    Source.Configurator.AddStep(OperationFeedback);
+    var Feedback = Source.CreateFeedback();
+
+    Feedback.UseShouldHaveBeen();
+
+    ActionSurfaceMock.Operation1CallCount.Should().Be(0);
+    ActionSurfaceMock.Operation2CallCount.Should().Be(0);
+    RequiresMore.Value.Should().Be(false);
+  }
+}
---
+++ src/ThoughtSharp.Runtime/AccumulatedUseFeedback.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Immutable;
+using System.Linq.Expressions;
+
+namespace ThoughtSharp.Runtime;
+
+public static class AccumulatedUseFeedback
+{
+  public static FeedbackSource<AccumulatedUseFeedbackConfigurator<T>, AccumulatedUseFeedback<T>> GetSource<T>()
+  {
+    var Configurator = new AccumulatedUseFeedbackConfigurator<T>();
+    return new(Configurator, Configurator.CreateFeedback);
+  }
+
+  public static void NoOp<T>(T _)
+  {
+  }
+}
+
+public class AccumulatedUseFeedbackConfigurator<T>
+{
+  readonly List<UseFeedback<T>> Steps = [];
+
+  public AccumulatedUseFeedback<T> CreateFeedback()
+  {
+    return new([..Steps]);
+  }
+
+  public void AddStep(UseFeedback<T> Step)
+  {
+    Steps.Add(Step);
+  }
+}
+
+public class AccumulatedUseFeedback<T>(ImmutableArray<UseFeedback<T>> Steps)
+{
+  public void UseShouldHaveBeen(params IEnumerable<Expression<Action<T>>> Actions)
+  {
+    Steps.First().ExpectationsWere((_, More) => More.Value = false);
+  }
+}
