816495e22ed41e4927ac045d6c532b43a3c427bf
Max Guernsey, III <max@hexsw.com>
2025-06-02 22:16:26 -0700

    .

---
+++ src/ThoughtSharp.Scenarios.Model/MindPool.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Immutable;
+
+namespace ThoughtSharp.Scenarios.Model;
+
+public class MindPool(ImmutableDictionary<Type, MindPlace> Places)
+{
+  public object GetMind(Type MindType)
+  {
+    var Place = Places[MindType];
+    var Brain = Place.MakeNewBrain();
+
+    return Place.MakeNewMind(Brain);
+  }
+}
