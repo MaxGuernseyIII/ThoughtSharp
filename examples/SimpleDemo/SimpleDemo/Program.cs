61db0b8646f48922e573fafc156805bd38400641
Max Guernsey, III <max@hexsw.com>
2025-06-03 12:26:11 -0700

    do a better job of publishing analyzers by not saying they're analyzers...

---
+++ examples/SimpleDemo/SimpleDemo/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using ThoughtSharp.Runtime;
+
+Console.WriteLine("Hello, World!");
+
+[Mind]
+public partial class TheMind
+{
+  [Make]
+  public partial CognitiveResult<D, D> MakeIt();
+}
+
+[CognitiveData]
+public partial class D
+{
+  
+}
