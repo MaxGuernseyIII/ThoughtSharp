b7d876555ebebdc98da8e72fb59570c50322b2eb
Max Guernsey, III <max@hexsw.com>
2025-06-05 10:21:38 -0700

    working on shaping tool

--- src/dotnet-train/Program.cs
+++ src/dotnet-train/Program.cs
@@ -1,15 +1,95 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// MIT License
+// 
+// Copyright (c) 2025-2025 Hexagon Software LLC
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
-using CommandLine;
-using dotnet_train;
+using System.CommandLine;
+using System.CommandLine.Parsing;
 
-Parser.Default.ParseArguments<Options>(args)
-  .WithParsed(Args =>
+var RootCommand = new RootCommand("Train models with ThoughtSharp");
+var TargetArgument = new Argument<FileInfo?>(
+    "--target",
+    description: "The .sln, .csproj, or .dll to train",
+    isDefault: true,
+    parse: A =>
+    {
+      var ToTrain = GetFileSystemObjectToTrain(A);
+
+      if (ToTrain is FileInfo R)
+        return R;
+
+      if (ToTrain is DirectoryInfo D)
+        return ResolveDirectoryToFile(D, A);
+
+      return null;
+    })
   {
-    Console.WriteLine($"You want me to parse {Args.RelativeAssemblyPath}");
-  })
-  .WithNotParsed(_ =>
+    Arity = ArgumentArity.ZeroOrOne
+  }
+  .AddCompletions(C =>
   {
-    Console.Error.WriteLine("Your arguments weren't understood. Try --help.");
-    Environment.ExitCode = -1;
+    return Directory
+      .EnumerateFileSystemEntries(Directory.GetCurrentDirectory(), "*.*", SearchOption.TopDirectoryOnly)
+      .Select(F => Path.GetFileName(F)!)
+      .Where(F => F.StartsWith(C.WordToComplete, StringComparison.OrdinalIgnoreCase))
+      .Where(F => F.EndsWith(".sln", StringComparison.OrdinalIgnoreCase) ||
+                  F.EndsWith(".csproj", StringComparison.OrdinalIgnoreCase) ||
+                  F.EndsWith(".dll", StringComparison.OrdinalIgnoreCase));
   });
+
+RootCommand.AddArgument(TargetArgument);
+
+RootCommand.SetHandler(ToTrain =>
+{
+  if (ToTrain is not null)
+    Console.WriteLine($"You want me to train {ToTrain.FullName}");
+  else
+    Console.WriteLine("You want me to train whatever is in this directory.");
+}, TargetArgument);
+
+Environment.ExitCode = RootCommand.Invoke(args);
+
+static FileInfo? ResolveDirectoryToFile(DirectoryInfo Directory, ArgumentResult Result)
+{
+  var Options = Directory.GetFiles("*.sln").Concat(Directory.GetFiles("*.csproj"));
+  FileInfo? Inferred = null;
+  if (!Options.Any())
+    Result.ErrorMessage = "No solution or project files found in that directory";
+  else if (Options.Take(2).Count() > 1)
+    Result.ErrorMessage =
+      $"Found too many files:{string.Join("", Options.Select(O => Environment.NewLine + "  - " + O.FullName))}";
+  else
+    Inferred = Options.Single();
+
+  return Inferred;
+}
+
+FileSystemInfo? GetFileSystemObjectToTrain(ArgumentResult ArgumentResult)
+{
+  if (!ArgumentResult.Tokens.Any()) 
+    return null;
+
+  var Path = ArgumentResult.Tokens.Single().Value;
+
+  if (Directory.Exists(Path))
+    return new DirectoryInfo(Path);
+
+  return new FileInfo(Path);
+}
