bc4577b0dd9a898839a6848fa77ef7cd6bccfdb0
Max Guernsey, III <max@hexsw.com>
2025-06-21 18:48:55 -0700

    get rid of unused using

--- src/ThoughtSharp.Runtime/Inference.cs
+++ src/ThoughtSharp.Runtime/Inference.cs
@@ -20,8 +20,6 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
-using System.Collections.Immutable;
-
 namespace ThoughtSharp.Runtime;
 
 public interface Inference : IDisposable, InferenceSource
