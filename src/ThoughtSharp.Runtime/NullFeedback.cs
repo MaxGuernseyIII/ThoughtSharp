ebcb6167e20a136b4de5c6739fbf82540b5bd10a
Max Guernsey, III <max@hexsw.com>
2025-07-01 12:26:38 -0700

    more documentation

--- src/ThoughtSharp.Runtime/NullFeedback.cs
+++ src/ThoughtSharp.Runtime/NullFeedback.cs
@@ -1,8 +1,17 @@
-﻿namespace ThoughtSharp.Runtime;
+﻿using JetBrains.Annotations;
 
+namespace ThoughtSharp.Runtime;
+
+/// <summary>
+/// A dummy object used for when feedback is not supported.
+/// </summary>
+[PublicAPI]
 public class NullFeedback
 {
   NullFeedback() {}
 
+  /// <summary>
+  /// The only instance.
+  /// </summary>
   public static NullFeedback Instance { get; } = new();
 }
